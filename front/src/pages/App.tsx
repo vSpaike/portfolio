a0bb5055dd2b2f9318854ebe2022ad214ad8fc71
Jeremy <demon.e2200662@etud.univ-ubs.fr>
2026-03-17 08:49:10 +0100

    add: background and glass

--- front/src/pages/App.tsx
+++ front/src/pages/App.tsx
@@ -1,65 +1,21 @@
-import React from 'react'
+import mountainBackground from './assets/mountain_background.jpg'
 
-function Navbar() {
+function Background() {
   return (
-    <ul className="menu menu-horizontal bg-base-200 rounded-box mt-6">
-      <li>
-        <a className="tooltip" data-tip="Home">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor">
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-      </svg>
-    </a>
-  </li>
-  <li>
-    <a className="tooltip" data-tip="Details">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-5 w-5"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor">
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-      </svg>
-    </a>
-  </li>
-  <li>
-    <a className="tooltip" data-tip="Stats">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-5 w-5"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor">
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-      </svg>
-    </a>
-  </li>
-</ul>
-);
+    <div className="fixed inset-0 -z-10">
+      <img src={mountainBackground} alt="Background" className="h-full w-full object-cover" />
+    </div>
+  )
 }
 
 function App() {
   return (
     <>
-      <div className='flex flex-col items-center justify-center h-screen'>
-        <p>Cookie</p>   
+      <Background />
+      <div className="relative min-h-screen">
+        <div className="glass absolute inset-y-20 left-1/2 -translate-x-1/2 w-5/6 rounded-lg text-white">
+          <h1 className="p-64 text-4xl font-bold mb-4">Hi, I'm John Doe</h1>
+        </div>
       </div>
     </>
   )
