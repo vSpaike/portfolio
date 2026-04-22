c78efab5c28374094d76c3eb391cfe51ee0ec168
Jeremy <demon.e2200662@etud.univ-ubs.fr>
2026-04-22 11:23:28 +0200

    tests colors

--- front/src/pages/Accueil.tsx
+++ front/src/pages/Accueil.tsx
@@ -10,13 +10,15 @@ function Background() {
         position: [0.25, -0.35, 0],
         size: 50,
         title: "Mon étoile 1",
-        onClick: () => console.log("Étoile 1 cliquée")
+        onClick: () => console.log("Étoile 1 cliquée"),
+        background: "rgba(255,255,255,1) 0%, rgba(120,244,196,0.95) 45%, rgba(120,232,120,0.5) 70%, rgba(120,232,120,0) 100%"
         },
         {
           position: [-0.3, 0.4, 0],
           size: 30,
           title: "Mon étoile 2",
-          onClick: () => console.log("Étoile 2 cliquée")
+          onClick: () => console.log("Étoile 2 cliquée"),
+          background: "rgba(255,255,255,1) 0%, rgba(120,20,196,0.95) 45%, rgba(120,20,120,0.5) 70%, rgba(120,20,120,0) 100%"
         }
       ]}
     />
